9ca434c007c06f7bfadc7b9e2473ac019f6faf84
aimendez <aimendez@uc.cl>
2021-03-12 14:06:49 +0100

    asset list updated

--- main/app.py
+++ main/app.py
@@ -10,12 +10,12 @@ api_ = tradeapi.REST(  os.environ['ALPACA_KEY_ID'] ,
 					   base_url = "https://paper-api.alpaca.markets" )
 
 assets = api_.list_assets()
-df_assets = pd.DataFrame.from_dict(
-								   { 'symbol':  [ asset.symbol for asset in assets if asset.tradable == True],
-						 			 'name':[ asset.name for asset in assets if asset.tradable == True],
-						 			 'exchange':[ asset.exchange for asset in assets if asset.tradable == True],
-						 			 'status':[ asset.status for asset in assets if asset.tradable == True],
-									}).to_csv('./assets/asset_list.csv')
+df_assets = pd.DataFrame(
+						   { 'symbol':  [ asset.symbol for asset in assets if asset.tradable == True],
+				 			 'name':[ asset.name for asset in assets if asset.tradable == True],
+				 			 'exchange':[ asset.exchange for asset in assets if asset.tradable == True],
+				 			 'status':[ asset.status for asset in assets if asset.tradable == True],
+							}).to_csv('./assets/asset_list.csv')
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.FLATLY,dbc.themes.GRID])
 server = app.server
